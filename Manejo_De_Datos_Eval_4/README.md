9a3ffbf5dcfcb621fc8ff0748c2669a693ebe367
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2024-07-24 10:06:21 -0400

    uwu

--- Manejo_De_Datos_Eval_4/README.md
+++
@@ -1,2 +0,0 @@
-# data_analysis
-Proyect about data analysis
--- Manejo_De_Datos_Eval_4/primeros.py
+++
@@ -1,9 +0,0 @@
-#Importando Librerias Necesarias
-import pandas as pd
-import matplotlib.pyplot as plt
-
-#Asignando el model y filtrando las primeras lineas
-model = pd.read_csv("Manejo_De_Datos_Eval_4/model.txt", delim_whitespace=True, skiprows = 3,parse_dates = {'Timestamp': [0, 1]}, index_col = 'Timestamp')
-
-pd.plotting.scatter_matrix(model.loc[model.index[:1000], 'M(m/s)':'D(deg)'])
-plt.show()
